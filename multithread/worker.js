81fdd82d3c2a940fdb2d8f1f62ddcd4bf4aacfd8
Vladimir Mandic <mandic00@live.com>
2021-10-05 21:10:31 -0400

    additional optimizations

--- multithread/worker.js
+++ multithread/worker.js
@@ -2,26 +2,19 @@ const threads = require('worker_threads');
 
 let debug = false;
 
-/**@type SharedArrayBuffer | null */
-let buffer = null;
-/**@type Float64Array | null */
-let view = null;
+/**@type SharedArrayBuffer */
+let buffer;
+/**@type Float64Array */
+let view;
 let threshold = 0;
 let records = 0;
 
 const descLength =  8 * 1024; // descriptor length in bytes: 8-bytes per f64 element with 1024-elements 
 
-const getDescriptor = (index) => {
-  if (!view) return [];
-  const descriptor = [];
-  for (let i = 0; i < 1024; i++) descriptor.push(view[index * descLength + i]);
-  return descriptor;
-}
-
-function distance(descriptor1, descriptor2, options = { order: 2 }) {
+function distance(descriptor1, index, options = { order: 2 }) {
   let sum = 0;
   for (let i = 0; i < descriptor1.length; i++) {
-    const diff = (options.order === 2) ? (descriptor1[i] - descriptor2[i]) : (Math.abs(descriptor1[i] - descriptor2[i]));
+    const diff = (options.order === 2) ? (descriptor1[i] - view[index * descLength + i]) : (Math.abs(descriptor1[i] - view[index * descLength + i]));
     sum += (options.order === 2) ? (diff * diff) : (diff ** options.order);
   }
   return sum;
@@ -31,12 +24,12 @@ function match(descriptor, options = { order: 2 }) {
   let best = Number.MAX_SAFE_INTEGER;
   let index = -1;
   for (let i = 0; i < records; i++) {
-    const res = distance(descriptor, getDescriptor(i), { order: options.order });
+    const res = distance(descriptor, i, { order: options.order });
     if (res < best) {
       best = res;
       index = i;
     }
-    if (best < threshold) break;
+    if (best < threshold || best === 0) break; // short circuit
   }
   best = (options.order === 2) ? Math.sqrt(best) : best ** (1 / options.order);
   return { index, distance: best, similarity: Math.max(0, 100 - best) / 100.0 };
@@ -48,6 +41,7 @@ threads.parentPort?.on('message', (msg) => {
     const result = match(msg.descriptor);
     const t1 = performance.now();
     threads.parentPort?.postMessage({ request: msg.request, time: Math.trunc(t1 - t0), ...result });
+    return; // short circuit
   }
   if (msg instanceof SharedArrayBuffer) { // called only once to receive reference to shared array buffer
     buffer = msg;
